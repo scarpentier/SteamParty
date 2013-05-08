8760002ea9ec20551a488f3f54b024d86f4cf54c
Simon Carpentier <simon.carpentier@spacebar.ca>
2013-05-08 16:21:35 -0400

    Initial web project

---
+++ Web/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SteamParty.Web.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server" Width="564px">76561197975995523,76561197962208538</asp:TextBox><asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    </div>
+        <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+    </form>
+</body>
+</html>
---
+++ Web/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SteamParty.Core;
+
+namespace SteamParty.Web
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var steamids = TextBox1.Text.Split(',');
+
+            var api = new SteamApi(ConfigurationManager.AppSettings["ApiKey"]);
+
+            var comparer = new Comparer(api);
+
+            var games = comparer.Compare(Array.ConvertAll(steamids, long.Parse));
+
+            var sb = new StringBuilder();
+            sb.Append("<table>");
+
+            foreach (var game in games)
+            {
+                sb.Append(string.Format("<tr><td>{0}<br/><img src='{1}'/></td><td>", game.Key.AppId, game.Key.LogoUrl));
+                foreach (var player in game.Value)
+                {
+                    sb.Append(string.Format("<img src='{0}' />", player.AvatarUrl64));
+                }
+                sb.Append("</td></tr>");
+            }
+
+            sb.Append("</table>");
+            Literal1.Text = sb.ToString();
+        }
+
+
+    }
+}
